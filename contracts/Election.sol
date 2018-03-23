4eed17dc4cd924d30c4788c17f60b148ed10c1f8
“Hassan <hassan.almandeel@gmail.com>
2018-03-22 17:31:07 -0700

    add candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -10,10 +10,19 @@ contract Election {
 
   // Store candidates
   // Fetch candidate
+  mapping(uint => Candidate) public candidates;
   // Store Candidates Counts
+  uint public candidatesCount;
 
-  // constructor
+  // constructor, note: constructor is a function that dosent have name, instead it refrences the contract name (ie Election)
   function Election () public {
+    addCandidate('Trump');
+    addCandidate('Clinton');
+  }
 
+  // add candidates
+  function addCandidate (string _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
