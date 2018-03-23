c9660ce5ba2a9abd498fcbc54f48d3b924e64c07
“Hassan <hassan.almandeel@gmail.com>
2018-03-22 17:08:40 -0700

    Model a candidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,19 @@
 pragma solidity ^0.4.2;
 
 contract Election {
+  // Model a candidate
+  struct Candidate {
+    unit id;
+    string name;
+    unit voteCount;
+  }
+  
   // Store candidates
-  // Read candidates
-  string public candidate;
+  // Fetch candidate
+  // Store Candidates Counts
+
   // constructor
   function Election () public {
-    candidate = 'Trump';
+
   }
 }
