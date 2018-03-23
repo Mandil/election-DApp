99f4a678dcc188beea5bfc9bebccb0fbe6afe9be
“Hassan <hassan.almandeel@gmail.com>
2018-03-22 21:44:02 -0700

    write test to verify candidates with currect value

--- test/election.js
+++ test/election.js
@@ -10,4 +10,20 @@ contract('Election', function(accounts) {
     });
   });
 
+  it('initializes candidates with currect value', function(){
+    return Election.deployed().then(function(instance){
+      electionInstance = instance;
+      return electionInstance.candidates(1);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 1, 'has the correct id');
+      assert.equal(candidate[1], 'Trump', 'has the correct name');
+      assert.equal(candidate[2], 0, 'has the correct voting count');
+      return electionInstance.candidates(2);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 2, 'has the correct id');
+      assert.equal(candidate[1], 'Clinton', 'has the correct name');
+      assert.equal(candidate[2], 0, 'has the correct voting count');
+    });
+  });
+
 });
